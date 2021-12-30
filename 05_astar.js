bcc0d5f795a470a8ddf128c5b87e2b20576b4010
Emely Garcia <emely.g@firstlight.ai>
2021-12-30 14:44:59 -0600

    star algoritm

--- 05_astar.js
+++ 05_astar.js
@@ -24,7 +24,7 @@ function bestfirst(start, end){
 	var cont = 0
 	var dot = '{'
 	var list = [[start,heuristic(start, end),inc(),0]];
-	dot+=list[0][2]+' [label="'+list[0][0]+'"];'
+	dot+=list[0][2]+`[label="${getPrintData(list[0][0][0])} | ${getPrintData(list[0][0][1])} | ${getPrintData(list[0][0][2])} \n ${getPrintData(list[0][0][3])} | ${getPrintData(list[0][0][4])} | ${getPrintData(list[0][0][5])} \n ${getPrintData(list[0][0][6])} | ${getPrintData(list[0][0][7])} | ${getPrintData(list[0][0][9])}"];`;
 	while (list.length > 0){		
 		var current = list.shift();
 		if (current[0] == end) {			
@@ -33,7 +33,7 @@ function bestfirst(start, end){
 		}		
 		var temp = successors(current, end);
 		//temp.reverse();
-		temp.forEach(val => dot+=val[2]+' [label="'+val[0]+'"];'+current[2]+'--'+val[2]+' [label="'+(val[1]-current[3]-1)+'+'+(current[3]+1)+'"] ;')
+		temp.forEach(val => dot+=val[2]+`[color="${val[0] === '12345678' ? '#7BE141': ''}" label="${getPrintData(val[0][0])} | ${getPrintData(val[0][1])} | ${getPrintData(val[0][2])} \n ${getPrintData(val[0][3])} | ${getPrintData(val[0][4])} | ${getPrintData(val[0][5])} \n ${getPrintData(val[0][6])} | ${getPrintData(val[0][7])} | ${getPrintData(val[0][9])}"];`+current[2]+'--'+val[2]+';')
 		list = list.concat(temp);
 		list = list.sort( function(a,b) { return a[1] - b[1] });
 		cont++
@@ -52,9 +52,13 @@ function inc() {
 	return id++
 }
 
+function getPrintData(data) {
+	return data ? data : '  ';
+}
+
 function puzzle() {
-	var nodes = prompt("Ingrese texto inicial y texto final separados por un espacio")
-	if (nodes == null || nodes == '') nodes = 'halo hola'
+	var nodes //= prompt("Ingrese texto inicial y texto final separados por un espacio")
+	if (nodes == null || nodes == '') nodes = '14253678 12345678'
 	nodes = nodes.split(' ')
 	return bestfirst(nodes[0], nodes[1])
 }
